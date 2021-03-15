4cbd406b8b94b955618f5bc589cbd9f09c07c0c1
Alvaro Lopez Medina <alopezme@redhat.com>
2021-03-16 00:47:48 +0100

    Add workshop creation and clean code

--- auto-install.sh
+++ auto-install.sh
@@ -25,6 +25,7 @@ oc patch clusterrolebinding.rbac self-provisioners -p '{"subjects": null}'
 oc patch clusterrolebinding.rbac self-provisioners -p '{ "metadata": { "annotations": { "rbac.authorization.kubernetes.io/autoupdate": "false" } } }'
 
 # Avoid needing -c command on htpasswd
+rm -rf workshop-users.htpasswd
 touch workshop-users.htpasswd
 
 # Create projects for users
@@ -46,29 +47,39 @@ oc create secret generic workshop-htpass-secret -n openshift-config --from-file=
 
 if ! oc patch OAuth cluster --type=json --patch='[{"op": "test", "path": "/spec/identityProviders/1/htpasswd/fileData/name", "value": "workshop-htpass-secret"}]' &> /dev/null; then
     echo -ne "\nOAuth provider was not set. Configuring..."
-    oc patch OAuth cluster --type=json --patch='[{"op": "add", "path": "/spec/identityProviders/-", "value": {"name": "WORKSHOP","challenge": true,"login": true,"mappingMethod": "claim","type": "HTPasswd","htpasswd": {"fileData": {"name": "workshop-htpass-secret"}}}}]'
+    oc patch OAuth cluster --type=json --patch='[{"op": "add", "path": "/spec/identityProviders/-", "value": {"name": "WORKSHOP LOGIN","challenge": true,"login": true,"mappingMethod": "claim","type": "HTPasswd","htpasswd": {"fileData": {"name": "workshop-htpass-secret"}}}}]'
     echo -e "[OK]\n"
 else
     echo -e "\nOAuth provider was set. Skip."
 fi
-
-# Use the following command to remove the OAuth provider if you want to uninstall
-# oc patch OAuth cluster --dry-run=client --type=json --patch='[\ 
-# {"op": "test", "path": "/spec/identityProviders/1/htpasswd/fileData/name", "value": "workshop-htpass-secret"},\
-# {"op": "remove", "path": "/spec/identityProviders/1"}]'
-
-
 
 
 ##
 # 2) ETHERPAD
 ## 
 echo -e "\Installing Etherpad in project 'etherpad'..."
-git clone https://github.com/alvarolop/etherpad-for-openshift.git && ./etherpad-for-openshift/deploy.sh
+git clone https://github.com/alvarolop/etherpad-for-openshift.git && cd etherpad-for-openshift && ./deploy.sh && cd -
 rm -rf ./etherpad-for-openshift
 
 
 ##
 # 3) DEPLOY WORKSHOP
+# This script automatically installs Pipelines workshop, but can be customizable for anything
+# https://github.com/openshift-labs/lab-tekton-pipelines/tree/4.6
 ## 
+WORKSHOP_PROJECT_NAME=workshop
+
+oc new-project $WORKSHOP_PROJECT_NAME --display-name "Pipelines Workshops"
+
+git clone --recurse-submodules --branch 1.0 https://github.com/openshift-labs/lab-tekton-pipelines.git
+cd lab-tekton-pipelines
+.workshop/scripts/deploy-spawner.sh
+cd ..
+rm -rf lab-tekton-pipelines
+
+HOMEROOM_ROUTE=$(oc get routes lab-tekton-pipelines -n $WORKSHOP_PROJECT_NAME --template='https://{{(index .items 0).spec.host }}')
+
+echo -e "\Available URLs:"
+# echo -e " * ETHERPAD URL: $HOMEROOM_ROUTE"
+echo -e " * HOMEROOM URL: $HOMEROOM_ROUTE"
 
